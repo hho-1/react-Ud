395cb1d09e394dfe587f0100ee4942f0eb51f429
Habip Hakkioglu <h.hakkioglu@icloud.com>
2023-08-24 19:45:19 +0200

    React Hooks have been studied.

---
+++ 03-React-Hooks/customhook-1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Sayac from './Sayac';
+import TitleOne from './TitleOne';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TitleOne /> */}
+      <Sayac />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03-React-Hooks/customhook-2/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import PersonalInfo from './PersonalInfo';
+import Sayac from './Sayac';
+import TitleOne from './TitleOne';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TitleOne /> */}
+
+      {/* <Sayac /> */}
+      <PersonalInfo />
+    </div>
+  );
+}
+
+export default App;
